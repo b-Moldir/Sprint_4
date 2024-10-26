e3027e8a589b1a48aaf1059c5fa459bd924adee8
Moldir Bektenova <bn.moldir@gmail.com>
2024-10-27 03:55:11 +0500

    Удалены файлы из ветки main

--- locators/main_page_locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class MainPageLocators:
-
-    QUESTION_LOCATOR = (By.XPATH, '//*[@id="accordion__heading-{}"]')
-    ANSWER_LOCATOR = (By.XPATH, '//div[{}] / div[2] / p')
-
